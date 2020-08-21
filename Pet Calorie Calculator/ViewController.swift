fed3e247b0f96241cc409c48b2255de604fa4d9f
Liang032 <s1287673784@gmail.com>
2020-08-21 10:17:38 +0930

    Initial Commit

---
+++ Pet Calorie Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pet Calorie Calculator
+//
+//  Created by Fan Liang on 21/8/20.
+//  Copyright © 2020 Fan Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
